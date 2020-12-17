025ada6f05eab5a203b162001db2a96f46b598c2
j-mello <mello.julien@gmail.com>
2020-12-17 12:15:23 +0100

    Fix missing typing in user

--- api/Entities/User.ts
+++ api/Entities/User.ts
@@ -2,14 +2,14 @@ export class User {
     email: string;
     nom: string;
     prenom: string;
-    birdday: Date;
+    birthday: Date;
     password: string
 
-    constructor(prenom, nom, email, birdday, password) {
+    constructor(prenom: string, nom: string, email: string, birthday: Date, password: string) {
         this.email = email;
         this.nom = nom;
         this.prenom = prenom;
-        this.birdday = birdday;
+        this.birthday = birthday;
         this.password = password;
     }
 
@@ -18,7 +18,7 @@ export class User {
         if (!regexEmail.test(this.email)) return false;
         if (this.prenom.trim() === "" || this.nom.trim() === "") return false;
         let currentDate = new Date();
-        if (currentDate.getTime() - this.birdday.getTime() < 13 * 365.25 * 24 * 60 * 60 * 1000) return false;
+        if (currentDate.getTime() - this.birthday.getTime() < 13 * 365.25 * 24 * 60 * 60 * 1000) return false;
         if (this.password.length < 8 || this.password.length > 40) return false;
         return true;
     }
